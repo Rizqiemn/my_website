22f701ccb2aa3d6e74668d3481a69a31c73d28a9
Rizqie Miftah <rizqiesempurna@gmail.com>
2018-10-16 11:42:52 +0700

    wahwaaahh

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Selamat datang di Web Kami</title>
+	<title>Selamat datang di Web Kamis</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
